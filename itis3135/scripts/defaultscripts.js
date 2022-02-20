3a64f4ddd8343ab89e1bd0a8d6a55f1f77edf0f0
sashpyndi <sashankpyndi@gmail.com>
2022-02-20 17:25:10 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,4 +1,11 @@
+function showDateAndTime(){
+   let currentdate = new Date(); 
+   currentdate.getMonth();
+   currentdate.getDay();
+   
+   let dateAndTime = "Today is " +  currentdate + " on " + currentdate.getMonth() + currentdate.getDay();
 
+} 
 
 function scriptTest(){
    alert("Hey my script is running");
