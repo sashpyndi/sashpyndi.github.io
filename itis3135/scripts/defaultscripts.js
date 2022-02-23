e1a5984838c6082ee71d79f931d963e358f5f6b3
sashpyndi <sashankpyndi@gmail.com>
2022-02-23 17:55:08 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,9 +1,10 @@
 function DateAndTime(){
    let today = new Date();
-   let month = today.toLocaleString('default', { month: 'long' });
+   var monthName = ["January", "February", "March", "April", "May", "June", "July" ,"August", "September", "October", "November", "December"];
+   let month = monthName[today.getMonth()];
    let year = today.getFullYear();
    let date  = today.getDate();
-   const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+   var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
    let day = weekday[today.getDay()];
    let hours = today.getHours();
    let minutes = today.getMinutes();
