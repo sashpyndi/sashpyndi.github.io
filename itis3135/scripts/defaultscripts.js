10de30c6812bb8d0d89f9f1b67655e8f6efbbf87
sashpyndi <sashankpyndi@gmail.com>
2022-02-23 16:45:14 -0500

    changed

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,15 +1,4 @@
-function showDateAndTime(){
-   let today = new Date(); 
-   let month = today.getMonth()+1;
-   let year = today.getFullYear();
-   let date = today.getDate();
 
-   let time = today.getTime();
-
-  
-
-  document.getElementById("DateAndTime").innerHTML = today;
-} 
 
 function scriptTest(){
    alert("Hey my script is running");
