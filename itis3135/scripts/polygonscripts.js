ce06a52b4364b4f87941e0e1c918805969a9e219
sashpyndi <sashankpyndi@gmail.com>
2022-02-26 15:37:10 -0500

    made changes

--- itis3135/scripts/polygonscripts.js
+++ itis3135/scripts/polygonscripts.js
@@ -3,7 +3,9 @@ function popUpMessage(){
     //alert(" The Salty Penguin would like your number between 1-10");
     let input;
     let prompt = prompt("The Salty Penguin would like your nujmber between 1-10");
-
+     if(prompt <1 || prompt > 10){
+        input = "Sorry invalid entry";
+     }
      document.getElemendById("prompt").innerHTML= text;
 }
 
