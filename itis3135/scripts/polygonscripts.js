7e643e649f2e0666a4a89aad52b2215980d332ac
sashpyndi <sashankpyndi@gmail.com>
2022-02-26 15:29:09 -0500

    Update polygonscripts.js

--- itis3135/scripts/polygonscripts.js
+++ itis3135/scripts/polygonscripts.js
@@ -1,4 +1,9 @@
 function popUpMessage(){
-    alert(" The Salty Penguin would like your number between 1-10");
+
+    //alert(" The Salty Penguin would like your number between 1-10");
+    let input;
+    let prompt = prompt("The Salty Penguin would like your nujmber between 1-10");
+    
+
 }
 
