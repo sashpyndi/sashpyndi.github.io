e27cc07198c398d9cf236fcb4bea19a520822ec7
sashpyndi <sashankpyndi@gmail.com>
2022-02-26 16:37:25 -0500

    Update polygonscripts.js

--- itis3135/scripts/polygonscripts.js
+++ itis3135/scripts/polygonscripts.js
@@ -2,30 +2,40 @@ function popUpMessage(){
     
    let number = window.prompt("The Salty Penguin would like your number between 1-10");
    let shape;
-    switch(number){
-        case "1":
-            shape = "henagon";
+    switch(number.value){
+        case 1:
+            shape = window.prompt("Thats a henagon");
             break;
-         case "2":
+         case 2:
              shape = "digon";
              break;
-        case "3":
+        case 3:
             shape = "trigon";
-        case "4":
+            break;
+        case 4:
             shape = "tetragon";
-        case "5":
+            break;
+        case 5:
             shape = "pentagon";
-        case "6": 
+            break;
+        case 6: 
              shape = "hexagon";
-        case "7":
+             break;
+        case 7:
             shape = "heptagon";
-        case "8":
+            break;
+        case 8:
             shape = "octogon";
-        case "9":
+            break;
+        case 9:
             shape = "nonagon";
-        case "10":
+            break;
+        case 10:
            shape = "decagon";
-        default: alert("Please enter a valid entry");
+           break;
+        default: 
+        alert("Please enter a valid entry");
+        popUpMessage();
 
     }
          
