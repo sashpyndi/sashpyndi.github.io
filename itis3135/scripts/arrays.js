a25b277d124a0da0e92d5e55de38329bfdf6e617
sashpyndi <sashankpyndi@gmail.com>
2022-03-14 22:08:22 -0400

    made changes

--- itis3135/scripts/arrays.js
+++
@@ -1,9 +0,0 @@
-const persons = [];
-const salaries = [];
-
-function addSalary(){
-  let salary = document.getElementyById("salary").value;
-
-  let statement = "Your salary is " + salary;
-  document.getElementById("income").innerHTML = statement;
-}
