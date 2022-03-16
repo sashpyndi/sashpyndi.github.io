f96076801430e684dae4a255bc250aff44c642fc
sashpyndi <sashankpyndi@gmail.com>
2022-03-15 23:16:55 -0400

    changes

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,26 @@
+const persons = [];
+const salaries = [];
+
+function addSalary(){
+      let salary = document.getElementById("salary").value;
+       salaries.push(salary);
+      let person = document.getElementById("name").value;
+      persons.push(person);
+     
+      let statement = person + "has a salary of " + salary;
+      document.getElementById("income").innerHTML = statement;
+      
+ 
+
+}
+
+function displaySalary(){
+    let table =  "<table <tr><td>Employee:</td><td>Salary</td></tr>";
+
+  
+   
+}
+
+function displayResults(){
+
+}
