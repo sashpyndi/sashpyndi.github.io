64955e4ed25356f761aa1b4a494db7874a0f8738
sashpyndi <sashankpyndi@gmail.com>
2022-03-19 13:13:45 -0400

    added accessibility files

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -3,7 +3,7 @@ const salaries = [];
 
 function addSalary(){
 
-      let salary = document.getElementById("salary").value;
+      let salary = parseFloat(document.getElementById("salary").value);
       let person = document.getElementById("name").value;
       if(isNaN(salary)){
         window.alert("Sorry, enter a number");
@@ -12,6 +12,7 @@ function addSalary(){
         window.alert("Please Enter your salary");
       }
       else{
+        salary = parseFloat(salary);
         salaries.push(salary);
         persons.push(person);
       }
@@ -23,36 +24,34 @@ function addSalary(){
 
 
 function displayResults(){
-     let avg;
-     let highestSalary;
-     let highestSalaryPerson;
-     let total;
+     let avg=0;
+     let highestSalary=0;
+     let highestSalaryPerson=null;
+     let total=0;
+    
      
 
     
-     for(i=0; i<salaries.length; i++){
+     for( var i=0; i<salaries.length; i++){
          total +=salaries[i];
      }
-
-     avg = total/salaries.length;
-     document.getElementById()
-   for(i=0; i<salaries.length; i++){
-     if(salaries[i]>salaries[i+1]){
-         highestSalary = salaries[i];
+     let salaryLength = salaries.length;
+     avg = (total/salaryLength);
+    
+   for(var i=0; i<salaries.length; i++){
+     if(salaries[i]>highestSalary){
+         highestSalary = salaries[i] ;
          highestSalaryPerson = persons[i];
      }
-     else{
-         highestSalary = salaries[i+1];
-         highestSalaryPerson = persons[i+1];
-     }
+    
    }
-   document.getElementById("results").innerHTML = "The highest paid employee is " +  highestSalaryPerson + " with a salary of " + highestSalary 
+   document.getElementById("results").innerHTML = "The highest paid employee is " +  highestSalaryPerson + " with a salary of " + highestSalary +
    " The average of all the employees is   " + avg;
 }
 function displaySalary(){
     let table = '<table class = center<tr><td>Employee Name</td><td>Salary</td></tr>';
      
-    for( i = 0; i<salaries.length; i++){
+    for( var i = 0; i<salaries.length; i++){
          table+="<tr><td>"+persons[i]+"</td><td>"+salaries[i]+"</td></tr>";
     }
     
