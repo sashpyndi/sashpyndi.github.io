e0bcb5ef735bd3bb6f126d77d8120d853c714ddc
sashpyndi <sashankpyndi@gmail.com>
2022-04-15 21:36:36 -0400

    made changes

---
+++ itis3135/scripts/Activity10.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+        // preload the image for each link
+
+        // set up the event handlers for each link
+
+    			// get the image URL and caption for each image and animate the caption
+
+            // cancel the default action of each link
+
+
+    // move the focus to the first link
+
+}); // end ready
