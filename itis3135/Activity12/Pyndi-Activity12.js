c5cb66742b7b7f144733e9bf4edfaf38bcccef56
sashpyndi <sashankpyndi@gmail.com>
2022-04-19 18:28:04 -0400

    made changes

--- itis3135/Activity12/Pyndi-Activity12.js
+++ itis3135/Activity12/Pyndi-Activity12.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
     $("#slider").bxSlider({
+        randomStart: true,
+        moveSlides: 1,
         auto: true,
-        minSlides: 2,
-        maxSlides: 2,
-        slideWidth: 300,
-        slideMargin: 20
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20,
+        captions:true,
+        pause:3000,
+        pager:true,
+        pagerSelector: '#id_pager',
+        pagerType: "short"
     });
 });
