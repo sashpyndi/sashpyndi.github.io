62a4719008fbd6435b5a94e8a158877bf642ae4b
sashpyndi <sashankpyndi@gmail.com>
2022-04-20 21:37:09 -0400

    made changes

--- itis3135/Activity14/Pyndi-Activity14.js
+++ itis3135/Activity14/Pyndi-Activity14.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-	
+  $('#nav_list li').on('click',function(){
+    $.ajax({
+        type: "get",
+        url: "toobin.json"+
+        "sorkin.json"+
+         "sampson.json"+
+         "chua.json",
+
+        beforeSend: function() {
+            $("main").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("main").html("");
+            $.each(data,function(){
+               $.each(this,function(key,value){
+                   $("main").append(
+                       value.month + value.title + value.speaker + value.image+ value.text
+                       
+                   );
+               });
+            });
+        }
+    });
+  });
 }); // end ready
