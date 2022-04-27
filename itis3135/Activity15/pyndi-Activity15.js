4f1c78ce1826a04beec7966d3ce9ba39e1a04bd9
sashpyndi <sashankpyndi@gmail.com>
2022-04-26 20:28:23 -0400

    made changes

--- itis3135/Activity15/pyndi-Activity15.js
+++ itis3135/Activity15/pyndi-Activity15.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-
+   
+       
+      $.ajax({
+          type: "get",
+          url: "facultyList.json",
+  
+          beforeSend: function() {
+              $("#faculty").html("Loading...");
+          },
+          timeout: 10000,
+          error: function(xhr, status, error) {
+              alert("Error: " + xhr.status + " - " + error);
+          },
+          dataType: "json",
+          success: function(data) {
+              $("#faculty").html("");
+              $.each(data,function(){
+                 $.each(this,function(key,value){
+                     $("#faculty").append(
+                      "<img src = " + value.image + ">" + "<h2>"+ value.full_name + "</h2>" + "<h3>" + value.department + "</h3>" + "<p>" + value.bio + "</p>"
+                         
+                     );
+                 });
+              });
+          }
+      });
+    
 });
