746463d6d54c66aea3bb35598946fb675e9afc2d
sashpyndi <sashankpyndi@gmail.com>
2022-04-30 18:59:27 -0400

    made changes

--- itis3135/projects/JavaScript/submitform.js
+++ itis3135/projects/JavaScript/submitform.js
@@ -1,6 +1,11 @@
 
-function submitHelpform(){
-   
-    
+function submitHelpForm(){
+ let name = document.getElementById("fname").value;
+ let date = document.getElementById("date").value;
+ let time = document.getElementById("appt").value;
+  alert(name + ", Your total cost for this appointment is $2,000. We hope to see you soon on  " + date + " at "  + time);
+}
 
+function submitContactForm(){
+    let name = document.getElementById("f")
 }
