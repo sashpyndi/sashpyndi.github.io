6a960571bee2eae3890739a81e665d41467e59b1
sashpyndi <sashankpyndi@gmail.com>
2022-05-03 11:33:17 -0400

    made changes

---
+++ itis3135/projects/JavaScript/informationslider.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        randomStart: true,
+        moveSlides: 1,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20,
+        captions:true,
+        pause:3000,
+        pager:true,
+     
+    });
+});
