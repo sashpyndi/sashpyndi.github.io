397198ba28082e9922c5a267eb6632d589f19774
sashpyndi <sashankpyndi@gmail.com>
2022-05-04 23:31:23 -0400

    MADE CHANGES

---
+++ itis3135/projects/JavaScript/stdaccordin.js
@@ -0,0 +1,8 @@
+//<!-- jQuery call to the accordion() method. -->
+$(document).ready(function() {
+    $("#tabs").accordion({
+        collapsible:true,
+        active:false,
+       heightStyle:"content"
+    });
+});
