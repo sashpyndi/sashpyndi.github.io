d7ad0fe2032620c3bcb599ef5ad19469d738b66b
sashpyndi <sashankpyndi@gmail.com>
2022-05-07 15:20:30 -0400

    added json name data

---
+++ itis3135/projects/JavaScript/std's.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+   
+       
+    $.ajax({
+        type: "get",
+        url: "std's.json",
+
+        beforeSend: function() {
+            $("#STD").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#STD").html("");
+            $.each(data,function(){
+               $.each(this,function(key,value){
+                   $("#STD").append(
+                  "<h3>" + value.name + "<h3>"
+                       
+                   );
+               });
+            });
+        }
+    });
+  
+});
