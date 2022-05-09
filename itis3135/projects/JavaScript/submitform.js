0c0cc14c0dd8552ca2395305908e8b477a3a89fa
sashpyndi <sashankpyndi@gmail.com>
2022-05-09 18:17:13 -0400

    made changes

--- itis3135/projects/JavaScript/submitform.js
+++ itis3135/projects/JavaScript/submitform.js
@@ -1,25 +1,42 @@
 
 function submitHelpForm(){
  let name = document.getElementById("fname").value;
+ let lname = document.getElementById("lname").value;
+ let phoneNumber = document.getElementById("phoneNumber").value;
+ let emailAddress = document.getElementById("emailAddress").value;
+ let reason = document.getElementById("reason").value;
  let date = document.getElementById("date").value;
  let time = document.getElementById("appt").value;
-  alert(name + ", Your total cost for this appointment is $2,000. We hope to see you soon on  " + date + " at "  + time);
+ if(name == ""||lname == ""||phoneNumber == ""||emailAddress== "" || date == ""|| time ==""|| reason == "" ){
+     window.alert("Please fill all the blanks");
+ }
+ else{
+  window.alert(name + ", Your total cost for this appointment is $2,000. We hope to see you soon on  " + date + " at "  + time);
+ }
 }
 
 function submitContactForm(){
     let name = document.getElementById("firstName").value;
-    if(name = ""){
-        alert("Please Fill in your name");
+    let lastName = document.getElementById("lastName").value;
+    let subject = document.getElementById("subject").value;
+    if(name == "" || lastName == "" || subject == ""){
+        alert("Please Fill all the blanks");
     }
-    alert("I will get in contact with you soon " + name);
+    else{
+    window.alert("I will get in contact with you soon " + name);
+    }
 }
 
 function submitTestingForm(){
   let name = document.getElementById("FirstName").value;
+  let lastName = document.getElementById("LastName").value;
   let date = document.getElementById("Date").value;
   let time = document.getElementById("Appt").value;
-  if(name == " "||date == ""|| time == ""){
-      alert("Fill all the blanks");
+  if(name == " "||date == ""|| time == "" || lastName == ""){
+      alert("Please Fill all the blanks");
   }
-   alert(name + ", Your total cost for the test is  $1,000. We hope to see you soon on  " + date + " at "  + time);
+  else{
+      
+      window.alert(name + ", Your total cost for the test is  $1,000. We hope to see you soon on  " + date + " at "  + time);
+  }
 }
